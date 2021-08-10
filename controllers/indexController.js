3cbb5caad789852672c8fc080d0d1b7fd49610eb
LoanaRosi <85371029+LoanaRosi@users.noreply.github.com>
2021-08-09 21:53:25 -0300

    Add files via upload

---
+++ controllers/indexController.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const menu = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'menu.json'), 'utf-8'))
+
+
+module.exports = {
+    index : (req, res) => {
+        return res.render('index', {
+            menu
+        })
+    }
+}
